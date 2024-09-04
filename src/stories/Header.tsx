0c6165cc8804cf9a51bfe210b84637b7e679a773
Lila Papazian <lilapapazian@gmail.com>
2024-09-04 11:20:31 -0300

    add header story

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -3,17 +3,10 @@ import LogoML from "../assets/Logo_ML.png";
 import { SearchBox } from "./SearchBox";
 
 export const Header = () => {
-  // TODO: avoid losing the logo in smaller resolutions
-  // TODO: avoid searchbox overflowing the header
+  // TODO: avoid searchbox overflowing the header on smaller resolutions
   return (
     <div className='header'>
-      <div className='header__logo'>
-        <img
-          className='header__logo-img'
-          src={LogoML}
-          alt='Logo de MercadoLibre'
-        />
-      </div>
+      <img src={LogoML} alt='Logo de MercadoLibre' />
       <div className='header__search-box'>
         <SearchBox
           placeholder='Nunca dejes de buscar'
