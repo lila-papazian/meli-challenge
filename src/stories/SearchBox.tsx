026ff25089c4c0661d37bdf65db4d9fe0a4daf91
Lila Papazian <lilapapazian@gmail.com>
2024-09-03 22:58:56 -0300

    3 UI component search bar (#9)
    * add storybook + search box
    
    * rework the image imports
    
    * remove unused import

---
+++ src/stories/SearchBox.tsx
@@ -0,0 +1,22 @@
+import "./searchBox.scss";
+import IconSearch from "../assets/IconSearch.png";
+export interface SearchBoxProps {
+  placeholder: string;
+  ariaLabel: string;
+  onClick?: () => void;
+}
+
+export const SearchBox = ({ placeholder, ariaLabel }: SearchBoxProps) => (
+  <div className='search-input'>
+    <input
+      className='search-input__input'
+      type='search'
+      aria-label={ariaLabel}
+      minLength={3}
+      placeholder={placeholder}
+    />
+    <button className='search-input__icon' aria-label="Buscar">
+      <img src={IconSearch} alt="Buscar" />
+    </button>
+  </div>
+);
