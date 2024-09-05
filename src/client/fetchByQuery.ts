e78ff1115c70303aa407a7dd76dcd1d0ef647f88
Lila Papazian <lilapapazian@gmail.com>
2024-09-05 11:32:30 -0300

    28 results screen (#34)
    * finetune fetchByQuery endpoint
    
    * add search response type
    
    * refactor layout to avoid repetition
    
    * create custom hook
    
    * add items loader
    
    * add results route
    
    * results screen first approach
    
    * add loader for results screen
    
    * fix accessibility issues
    
    * add go home when clicking in logo
    
    * rework product card to pass item as prop directly
    
    * add null | undefined possibility
    
    * add maybe type

--- src/client/fetchByQuery.ts
+++ src/client/fetchByQuery.ts
@@ -1,8 +1,9 @@
+import { SearchResponseItem } from "../types/SearchResponse";
 import fetchData from "./utils";
 
-async function fetchByQuery(query: string) {
+async function fetchByQuery(query: string): Promise<SearchResponseItem[]> {
   const endpoint = "https://api.mercadolibre.com/sites/MLA/search";
-  const data = await fetchData(endpoint, { q: query });
+  const data = await fetchData(endpoint, { q: query, limit: "5" });
   return data ? data.results : [];
 }
 
