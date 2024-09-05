813b74fba48b9e00a34e9f35a24ecc4172b4b60a
Lila Papazian <lilapapazian@gmail.com>
2024-09-05 13:42:32 -0300

    fix filename import

--- src/client/endpoints/searchByQuery.ts
+++ src/client/endpoints/searchByQuery.ts
@@ -1,5 +1,5 @@
 import fetchByQuery from "../api/fetchByQuery";
-import mapApiToSearchByQuery from "../mappers/mapAPIToSearchByQuery";
+import mapApiToSearchByQuery from "../mappers/mapApiToSearchByQuery";
 
 const searchByQuery = async (query: string) => {
     const apiResponse = await fetchByQuery(query);
