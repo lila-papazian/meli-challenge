78867d08a9b93f3c8637c229f28edca0ceaa2d5d
Lila Papazian <lilapapazian@gmail.com>
2024-09-05 11:25:05 -0300

    add go home when clicking in logo

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -5,16 +5,17 @@ import { SearchBox } from "./SearchBox";
 export interface HeaderProps {
   onSearch: (query: string) => void;
   className?: string;
-  searchInput?: string | null;
+  searchInput?: string | null | undefined;
+
 }
 
 export const Header = ({ searchInput, className, onSearch }: HeaderProps) => {
   // TODO: avoid searchbox overflowing the header on smaller resolutions
   return (
     <div className={`header ${className}`}>
-      <div className='header__icon'>
+      <a className='header__icon' href="/">
         <img src={LogoML} alt='Logo de MercadoLibre' />
-      </div>
+      </a>
       <div className='header__search-box'>
         <SearchBox
           placeholder='Nunca dejes de buscar'
