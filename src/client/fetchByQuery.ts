58e54b573a136a7ec0815b9f84b3acca9462640e
Lila Papazian <lilapapazian@gmail.com>
2024-09-04 22:32:11 -0300

    finetune fetchByQuery endpoint

--- src/client/fetchByQuery.ts
+++ src/client/fetchByQuery.ts
@@ -2,7 +2,7 @@ import fetchData from "./utils";
 
 async function fetchByQuery(query: string) {
   const endpoint = "https://api.mercadolibre.com/sites/MLA/search";
-  const data = await fetchData(endpoint, { q: query });
+  const data = await fetchData(endpoint, { q: query, limit: "5" });
   return data ? data.results : [];
 }
 
