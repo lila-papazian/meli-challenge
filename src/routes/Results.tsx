ba4fcd5e769e0e4585d51854db494b898f6a9416
Lila Papazian <lilapapazian@gmail.com>
2024-09-06 11:39:03 -0300

    add no results option (#43)

--- src/routes/Results.tsx
+++ src/routes/Results.tsx
@@ -8,20 +8,24 @@ const Results = () => {
   const response = useLoaderData() as SearchByQueryResults;
   const navigate = useNavigate();
   const handleClick = (productId: string) => {
-    navigate( `/items/${productId}`);
+    navigate(`/items/${productId}`);
   };
 
   return (
     <Layout>
-      <ul role='tablist' className='results-screen'>
-        {response.items.map((item) => {
-          return (
-            <li role='tab' tabIndex={0} key={item.id}>
-              <ProductCard onClick={() => handleClick(item.id)} item={item} />
-            </li>
-          );
-        })}
-      </ul>
+      {response.items.length > 0 ? (
+        <ul role='tablist' className='results-screen'>
+          {response.items.map((item) => {
+            return (
+              <li role='tab' tabIndex={0} key={item.id}>
+                <ProductCard onClick={() => handleClick(item.id)} item={item} />
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div className="results-screen__no-results">No results found</div>
+      )}
     </Layout>
   );
 };
